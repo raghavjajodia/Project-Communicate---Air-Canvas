fc3dc80035ba2d0a26a4e2a20c08e1ef345ed48e
raghavjajodia <raghav_jajodia777@rediffmail.com>
2015-12-28 00:40:06 +0530

    first

---
+++ airCanvas/main.cpp
@@ -0,0 +1,120 @@
+#include <iostream>
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+
+using namespace cv;
+using namespace std;
+
+void boundTheBlob(Mat threshImg, Mat& originalImg)
+{
+	Mat temp;
+	threshImg.copyTo(temp);
+	vector< vector<Point> > contours;
+	vector<Vec4i> hierarchy;
+	findContours(temp, contours, hierarchy, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE);// retrieves external contours
+
+	/// Approximate contours to polygons + get bounding rects and circles
+	vector<vector<Point> > contours_poly(contours.size());
+	vector<Rect> boundRect(contours.size());
+	vector<Point2f>center(contours.size());
+	vector<float>radius(contours.size());
+	int k = 0;
+	for (int i = 0; i < contours.size(); i++)
+	{
+		approxPolyDP(Mat(contours[i]), contours_poly[i], 3, true);
+		Rect temp;
+		temp = boundingRect(Mat(contours_poly[i]));
+		if (temp.height>originalImg.rows / 25 && temp.width>originalImg.cols / 25)
+			boundRect[k++] = temp;
+		//minEnclosingCircle( (Mat)contours_poly[i], center[i], radius[i] );
+	}
+
+
+	/// Draw polygonal contour + bonding rects + circles
+	//Mat drawing = Mat::zeros(temp.size(), CV_8UC3 );
+	for (int i = 0; i< k; i++)
+	{
+		Scalar color = Scalar(255, 255, 255);
+		rectangle(originalImg, boundRect[i].tl(), boundRect[i].br(), color, 2, 8, 0);
+
+	}
+
+
+
+
+}
+
+int main(int argc, char** argv)
+{
+	VideoCapture cap(0); //capture the video from web cam
+
+	if (!cap.isOpened())  // if not success, exit program
+	{
+		cout << "Cannot open the web cam" << endl;
+		return -1;
+	}
+
+	namedWindow("Control", CV_WINDOW_AUTOSIZE); //create a window called "Control"
+
+	int iLowH = 0;
+	int iHighH = 179;
+
+	int iLowS = 0;
+	int iHighS = 255;
+
+	int iLowV = 0;
+	int iHighV = 255;
+
+	//Create trackbars in "Control" window
+	cvCreateTrackbar("LowH", "Control", &iLowH, 179); //Hue (0 - 179)
+	cvCreateTrackbar("HighH", "Control", &iHighH, 179);
+
+	cvCreateTrackbar("LowS", "Control", &iLowS, 255); //Saturation (0 - 255)
+	cvCreateTrackbar("HighS", "Control", &iHighS, 255);
+
+	cvCreateTrackbar("LowV", "Control", &iLowV, 255); //Value (0 - 255)
+	cvCreateTrackbar("HighV", "Control", &iHighV, 255);
+
+	while (true)
+	{
+		Mat imgOriginal;
+
+		bool bSuccess = cap.read(imgOriginal); // read a new frame from video
+
+		if (!bSuccess) //if not success, break loop
+		{
+			cout << "Cannot read a frame from video stream" << endl;
+			break;
+		}
+
+		Mat imgHSV;
+
+		cvtColor(imgOriginal, imgHSV, COLOR_BGR2HSV); //Convert the captured frame from BGR to HSV
+
+		Mat imgThresholded;
+		//inRange(imgHSV, Scalar(22, 30, 96), Scalar(56, 255, 255), imgThresholded);  //smiley ball
+		// inRange(imgHSV, Scalar(101, 155, 66), Scalar(114, 253, 255), imgThresholded); //bottleCap
+		inRange(imgHSV, Scalar(iLowH, iLowS, iLowV), Scalar(iHighH, iHighS, iHighV), imgThresholded); //Threshold the image
+		//morphological opening (remove small objects from the foreground)
+		erode(imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)));
+		dilate(imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)));
+
+		//morphological closing (fill small holes in the foreground)
+		dilate(imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)));
+		erode(imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)));
+
+		boundTheBlob(imgThresholded, imgOriginal);
+
+		imshow("Thresholded Image", imgThresholded); //show the thresholded image
+		imshow("Original", imgOriginal); //show the original image
+
+		if (waitKey(30) == 27) //wait for 'esc' key press for 30ms. If 'esc' key is pressed, break loop
+		{
+			cout << "esc key is pressed by user" << endl;
+			break;
+		}
+	}
+
+	return 0;
+
+}
